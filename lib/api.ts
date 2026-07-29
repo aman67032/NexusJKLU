0c09415053055b200999a3f075f89989e0aa57a5
aman67032 <amanpratapsingh@jklu.edu.in>
2026-07-29 19:41:08 +0530

    check

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://nexus-jklu-backend.vercel.app';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
